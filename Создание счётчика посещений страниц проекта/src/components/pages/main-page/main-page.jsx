bf5c6767bec84d4c30bcbf9e94658a31a019716d
GlebZhidovich <gzhidovich@gmail.com>
2023-04-04 22:46:27 +0300

    feat: add builder to page

--- Создание счётчика посещений страниц проекта/src/components/pages/main-page/main-page.jsx
+++ Создание счётчика посещений страниц проекта/src/components/pages/main-page/main-page.jsx
@@ -1,25 +1,29 @@
-import React, {useEffect} from 'react';
-import About from '/src/components/blocks/about/about';
-import FeaturesList from '/src/components/blocks/features-list/features-list';
-import {textToUser} from '/src/const';
-import {SingletonCounterService} from '/src/services/counter.service';
+import React, { useEffect } from "react";
+import About from "/src/components/blocks/about/about";
+import FeaturesList from "/src/components/blocks/features-list/features-list";
+import { textToUser } from "/src/const";
+import { SingletonCounterService } from "/src/services/counter.service";
+import { BuilderPages } from "../../builders/builder-pages/builder-pages";
 
 const singletonCounterService = new SingletonCounterService();
 
-function MainPage({features}) {
-  const page = 'Main';
+function MainPage({ features }) {
+  const page = "Main";
 
   useEffect(() => {
     singletonCounterService.incSingleton(page);
   }, []);
 
-  return (
+  return BuilderPages.setPromo(
+    "Внимание! Акция! Подробности по телефону 8 900000000"
+  ).renderForm((text) => (
     <>
       {singletonCounterService.getSingleton(page) > 5 && <h2>{textToUser}</h2>}
+      {text}
       <About />
       <FeaturesList features={features} />
     </>
-  );
+  ));
 }
 
 export default MainPage;
--- Создание счётчика посещений страниц проекта/src/components/pages/order/order.jsx
+++ Создание счётчика посещений страниц проекта/src/components/pages/order/order.jsx
@@ -12,16 +12,17 @@ import {
   PriceLabel,
   PriceValue,
   ProductSwiper,
-  CheckboxLabel
+  CheckboxLabel,
 } from "./styles";
 import { SwiperSlide } from "swiper/react";
 import SwiperCore, { Pagination, Mousewheel, Scrollbar } from "swiper/core";
 import "swiper/swiper-bundle.min.css";
+import { BuilderPages } from "../../builders/builder-pages/builder-pages";
 SwiperCore.use([Mousewheel, Pagination, Scrollbar]);
 
 // Оформление заказа
 function Order({
-  products // список продуктов
+  products, // список продуктов
 }) {
   const [swiperRef, setSwiperRef] = useState(null);
   const [selectProductIds, setSelectProductIds] = useState([]);
@@ -48,67 +49,72 @@ function Order({
     Итого: ${fullPrice} руб.
     Доставка по адресу: ${address}.`);
   };
-  return products && products.length ? (
-    <StyledOrder as="form">
-      <LeftColumn>
-        <Panel marginBottom={20} paddingTop={24} paddingBottom={10}>
-          <Title as="h2" size={TitleSize.EXTRA_SMALL} marginBottom={12}>
-            Выберите продукты
-          </Title>
-          <CheckboxList
-            labelComponent={CheckboxLabel}
-            name={"select-products"}
-            isGridList={false}
-            options={products.map((product) => ({
-              value: product.id,
-              title: product.name
-            }))}
-            selectValues={selectProductIds}
-            onChange={setSelectProductIds}
-            onClickLabel={handleOnClickProduct}
-          />
-        </Panel>
-        <Panel>
-          <Title size={TitleSize.EXTRA_SMALL} marginBottom={24}>
-            Сделать заказ
-          </Title>
-          <AddressInput
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            placeholder="Введите адрес доставки"
-          />
-          <PriceLabel as="span">Цена</PriceLabel>
-          <PriceValue value={fullPrice} />
-          <Button
-            maxWidth
-            onClick={handleBuyClick}
-            disabled={!(selectProductIds.length && address)}
-          >
-            Купить
-          </Button>
-        </Panel>
-      </LeftColumn>
-      <ProductSwiper
-        onSwiper={setSwiperRef}
-        spaceBetween={12}
-        direction="vertical"
-        slidesPerView="auto"
-        scrollbar={{ draggable: true }}
-        mousewheel
-        pagination={{
-          type: "fanction"
-        }}
-      >
-        {products.map((product) => (
-          <SwiperSlide key={product.id}>
-            <ProductCart product={product} />
-          </SwiperSlide>
-        ))}
-      </ProductSwiper>
-    </StyledOrder>
-  ) : (
-    "Продукты были слишком вкусные и их разобрали."
-  );
+  return BuilderPages.setPromo(
+    "Внимание! Акция! Подробности по телефону 8 900000000"
+  ).renderForm((text) => {
+    return products && products.length ? (
+      <StyledOrder as="form">
+        <LeftColumn>
+          <Panel marginBottom={20} paddingTop={24} paddingBottom={10}>
+            <Title as="h2" size={TitleSize.EXTRA_SMALL} marginBottom={12}>
+              Выберите продукты
+              <h6>{text}</h6>
+            </Title>
+            <CheckboxList
+              labelComponent={CheckboxLabel}
+              name={"select-products"}
+              isGridList={false}
+              options={products.map((product) => ({
+                value: product.id,
+                title: product.name,
+              }))}
+              selectValues={selectProductIds}
+              onChange={setSelectProductIds}
+              onClickLabel={handleOnClickProduct}
+            />
+          </Panel>
+          <Panel>
+            <Title size={TitleSize.EXTRA_SMALL} marginBottom={24}>
+              Сделать заказ
+            </Title>
+            <AddressInput
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              placeholder="Введите адрес доставки"
+            />
+            <PriceLabel as="span">Цена</PriceLabel>
+            <PriceValue value={fullPrice} />
+            <Button
+              maxWidth
+              onClick={handleBuyClick}
+              disabled={!(selectProductIds.length && address)}
+            >
+              Купить
+            </Button>
+          </Panel>
+        </LeftColumn>
+        <ProductSwiper
+          onSwiper={setSwiperRef}
+          spaceBetween={12}
+          direction="vertical"
+          slidesPerView="auto"
+          scrollbar={{ draggable: true }}
+          mousewheel
+          pagination={{
+            type: "fanction",
+          }}
+        >
+          {products.map((product) => (
+            <SwiperSlide key={product.id}>
+              <ProductCart product={product} />
+            </SwiperSlide>
+          ))}
+        </ProductSwiper>
+      </StyledOrder>
+    ) : (
+      "Продукты были слишком вкусные и их разобрали."
+    );
+  });
 }
 
 export default Order;
